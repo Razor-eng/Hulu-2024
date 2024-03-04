a225e4a61c2b1881e5e15bb7e15c6d1258d9688a
Razor-eng <rajatmaharana64@gmail.com>
2024-03-04 22:35:20 +0530

    first commit

---
+++ src/Screen/Home.tsx
@@ -0,0 +1,20 @@
+import GenereMovieList from '../Components/GenereMovieList'
+
+
+import Header from '../Components/Header'
+import Hero from '../Components/Hero'
+
+function Home() {
+  return (
+    <div>
+      <Header />
+
+      <Hero />
+
+      <GenereMovieList />
+
+    </div>
+  )
+}
+
+export default Home
